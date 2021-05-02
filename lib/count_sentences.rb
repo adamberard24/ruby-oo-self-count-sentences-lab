99c67571872175ca97b34b74bea5598692e45ce5
Adam Schwartz <adamberard24@gmail.com>
2021-05-02 15:06:46 -0400

    commit tree 552605c7c62a85519a3d0d3ed5bfa78798d8f7f3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,30 @@ require 'pry'
 
 class String
 
-  def sentence?
+  def sentence? 
+    self.end_with?(".")
+   
+    end
+
     
-  end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    counter = 0
+    self.split(" ").each do |word|
+    if word.sentence? || word.question? || word.exclamation?
+      counter += 1
+    end
   end
+  counter
+end
 end
